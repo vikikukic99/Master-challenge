a74ece34114bc039d46e026857dd408144ee5e6a
Violeta Kukic <vkukic99@gmail.com>
2023-08-15 10:33:28 +0200

    css infrastructure

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,6 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+import "@/app/styles/global.scss"
+import "@/app/styles/fonts.scss"
 
 export const metadata = {
   title: 'Create Next App',
@@ -11,7 +10,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>{children}</body>
     </html>
   )
 }
